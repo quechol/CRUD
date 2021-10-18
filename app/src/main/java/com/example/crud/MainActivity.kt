3c8a4b7ad182f69023f43c3b7cc7145c64ab046a
quechol <zaratelogan@gmail.com>
2021-10-17 22:12:37 -0500

    login

--- app/src/main/java/com/example/crud/MainActivity.kt
+++ app/src/main/java/com/example/crud/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.crud
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnAcpt:Button = findViewById(R.id.btnLogin)
+        val edUser:EditText = findViewById(R.id.editUser)
+        val edPswd:EditText = findViewById(R.id.editPaswd)
+
+        var errores:Int = 0
+
+        btnAcpt.setOnClickListener{
+            val enviar = Intent(this, MenuInicial::class.java)
+        }
     }
 }
